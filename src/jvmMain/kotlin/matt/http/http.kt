aaea9ccfd6d6331f4bbbed3f1e0daa2369c1e582
mgroth0 <mgroth49@gmail.com>
2022-10-25 03:24:52 -0400

    autocommit

--- src/jvmMain/kotlin/matt/http/http.kt
+++ src/jvmMain/kotlin/matt/http/http.kt
@@ -2,14 +2,23 @@
 
 package matt.http
 
+import matt.file.MFile
 import matt.file.commons.USER_HOME
 import matt.http.url.MURL
+import matt.lang.ILLEGAL
+import matt.lang.delegation.provider
+import matt.lang.delegation.varProp
+import matt.lang.go
 import matt.log.tab
+import matt.prim.byte.efficientlyTransferTo
 import java.io.IOException
+import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
 import java.net.URLConnection
+import kotlin.concurrent.thread
+
 
 fun netIsAvailable(): Boolean {
   return try {
@@ -26,70 +35,140 @@ fun netIsAvailable(): Boolean {
 }
 
 
-fun http(
-  method: HTTPType,
-  url: MURL,
-  headers: Map<String, String> = mapOf(),
-  data: ByteArray? = null,
-  printResponse: Boolean = false
-): ByteArray {
+fun MURL.printHTTP(
+  op: HTTPRequest.()->Unit,
+) = httpString { op() }.go {
+  println(it)
+}
 
+fun MURL.httpString(
+  op: HTTPRequest.()->Unit,
+) = http { op() }.bufferedReader().readText()
 
-  val con: HttpURLConnection = url.jURL.openConnection() as HttpURLConnection
-  con.requestMethod = method.name
+fun MURL.http(
+  op: HTTPRequest.()->Unit = {},
+): InputStream {
+  val req = HTTPRequest(this)
+  req.op()
+  return req.connect()
+}
 
+@DslMarker
+annotation class HTTPDslMarker
 
+class HTTPRequest internal constructor(private val url: MURL) {
+  private val con = url.jURL.openConnection() as HttpURLConnection
+  var method: HTTPType
+	get() = con.requestMethod.let { m -> HTTPType.values().first { it.name == m } }
+	set(value) {
+	  con.requestMethod = value.name
+	}
 
-  headers.forEach {
-	con.setRequestProperty(it.key, it.value)
+  fun headers(op: HTTPHeaders.()->Unit) {
+	HTTPHeaders(con).apply(op)
   }
 
+  var verbose = false
 
+  var writer: Writer? = null
+	set(value) {
+	  require(field == null)
+	  field = value
+	}
 
+  var data: ByteArray
+	get() = ILLEGAL
+	set(value) {
+	  writer = BasicWriter(value)
+	}
 
-  println("sending ${method.name} to $url")
-  println("request properties:")
-  con.requestProperties.forEach {
-	tab("${it.key}:${it.value}")
+  fun writeAsync(file: MFile) {
+	writer = AsyncWriter(file)
   }
 
-  data?.let {
-	con.doOutput = true
-	con.outputStream.write(data)
+  internal fun connect(): InputStream {
+	if (verbose) {
+	  println("sending ${method.name} to $url")
+	  println("request properties:")
+	  con.requestProperties.forEach {
+		tab("${it.key}:${it.value}")
+	  }
+	}
+	writer?.go {
+	  it.write()
+	}
+	con.connect()
+	if (verbose) {
+	  println("Response = ${con.responseCode}")
+	  println("Message = ${con.responseMessage}")
+	}
+	con.errorStream?.readAllBytes()?.let {
+
+	  /*
+	  *
+	  *   * <p>This method will not cause a connection to be initiated.  If
+	  * the connection was not connected, or if the server did not have
+	  * an error while connecting or if the server had an error but
+	  * no error data was sent, this method will return null. This is
+	  * the default.
+	  *
+	  * @return an error stream if any, null if there have been no
+	  * errors, the connection is not connected or the server sent no
+	  * useful data.
+	  * */
+
+	  println("ERR: ${it.decodeToString()}")
+	} ?: run {
+	  if (verbose) {
+		println("no error message from server")
+	  }
+	}
+
+	return con.inputStream
+
   }
 
+  abstract inner class Writer {
+	internal abstract fun write()
+  }
 
-  println("Response = ${con.responseCode}")
-  println("Message = ${con.responseMessage}")
-
-
-
-  con.errorStream?.readAllBytes()?.let {
-
-	/*
-	*
-	*   * <p>This method will not cause a connection to be initiated.  If
-    * the connection was not connected, or if the server did not have
-    * an error while connecting or if the server had an error but
-    * no error data was sent, this method will return null. This is
-    * the default.
-    *
-    * @return an error stream if any, null if there have been no
-    * errors, the connection is not connected or the server sent no
-    * useful data.
-	* */
-
-	println("ERR: ${it.decodeToString()}")
-  } ?: run {
-	if (printResponse) {
-	  println("no error message from server")
+  inner class BasicWriter(private val bytes: ByteArray): Writer() {
+	override fun write() {
+	  con.doOutput = true
+	  con.outputStream.write(bytes)
 	}
   }
 
-  return con.inputStream.readAllBytes().also {
-	if (printResponse) {
-	  println("RESPONSE: ${it.decodeToString()}")
+  inner class AsyncWriter(private val file: MFile): Writer() {
+	override fun write() {
+	  con.doOutput = true
+	  thread {
+		println("running async data transfer")
+		file.channel().efficientlyTransferTo(con.outputStream)
+		println("finished running async data transfer")
+	  }
 	}
+  }
+}
+
+/*headers?*/
+@HTTPDslMarker
+class HTTPHeaders internal constructor(private val con: HttpURLConnection) {
+
+
+  var contentType: String by propProvider("Content-Type")
+  var accept: String by propProvider("Accept")
+  var auth: String by propProvider("Authorization")
+
+  private fun propProvider(key: String) = provider {
+	varProp(
+	  getter = {
+		con.getRequestProperty(key)
+	  },
+	  setter = {
+		con.setRequestProperty(key, it)
+	  }
+	)
   }
 
 }
@@ -101,3 +180,5 @@ object NetRC {
   val login = lines.first { it.startsWith("login") }.substringAfter("login").trim()
   val password = lines.first { it.startsWith("password") }.substringAfter("password").trim()
 }
+
+
