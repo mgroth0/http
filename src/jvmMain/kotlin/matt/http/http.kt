fe7744655c624a01e0d7741959233cfb77c0e555
mgroth0 <mgroth49@gmail.com>
2022-11-01 12:19:26 -0400

    autocommit

--- src/jvmMain/kotlin/matt/http/http.kt
+++ src/jvmMain/kotlin/matt/http/http.kt
@@ -19,20 +19,27 @@ import java.net.URL
 import java.net.URLConnection
 import kotlin.concurrent.thread
 
-
-fun netIsAvailable(): Boolean {
-  return try {
-	val url = URL("http://www.google.com")
-	val conn: URLConnection = url.openConnection()
-	conn.connect()
-	conn.getInputStream().close()
-	true
-  } catch (e: MalformedURLException) {
-	throw RuntimeException(e)
-  } catch (e: IOException) {
-	false
+object TheInternet {
+  /*ugh, won't work with gradle daemon. There has to be a way to set properties that are reset for the gradle daemon*/
+  val wasAvailableInThisRuntime by lazy {
+	isAvailable()
+  }
+  fun isAvailable(): Boolean {
+	return try {
+	  val url = URL("http://www.google.com")
+	  val conn: URLConnection = url.openConnection()
+	  conn.connect()
+	  conn.getInputStream().close()
+	  true
+	} catch (e: MalformedURLException) {
+	  throw RuntimeException(e)
+	} catch (e: IOException) {
+	  false
+	}
   }
 }
+
+
 
 
 fun MURL.printHTTP(
