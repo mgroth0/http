d69b451fd594365065c78b86392f73c3eff2d0e3
mgroth0 <mgroth49@gmail.com>
2023-01-18 11:52:17 -0500

    autocommit

--- src/jsMain/kotlin/matt/http/req/req.kt
+++ src/jsMain/kotlin/matt/http/req/req.kt
@@ -16,21 +16,19 @@ fun XMLHttpRequest.setOnReadyStateChange(op: (Event)->Unit) {
 }
 
 
-//val XMLHttpRequest.readyState get() = matt.http.resp.ReadyState.values()[readyState.toInt()]
-
 class HTTPRequester<T>(
   private val type: HTTPMethod,
   private val url: FileOrURL,
   private val responses: HTTPData.()->T
 ) {
 
-  @Suppress("FunctionName")
-  fun _setup(async: Boolean) = XMLHttpRequest().apply {
+  @PublishedApi
+  internal fun setup(async: Boolean) = XMLHttpRequest().apply {
 	open(type.name, url.toString(), async = async)
   }
 
-  @Suppress("FunctionName")
-  fun XMLHttpRequest._getResponses(): T = responses(
+  @PublishedApi
+  internal fun XMLHttpRequest.getResponses(): T = responses(
 	HTTPData(
 	  readyState = ReadyState.values()[readyState.toInt()],
 	  statusCode = status.toInt(),
@@ -40,35 +38,35 @@ class HTTPRequester<T>(
   )
 
   fun send(): T {
-	val req = _setup(async = false)
+	val req = setup(async = false)
 	req.send()
-	return req._getResponses()
+	return req.getResponses()
   }
 
   fun sendAsync(callback: (T)->Unit) {
-	val req = _setup(async = true)
+	val req = setup(async = true)
 	req.setOnReadyStateChange {
 	  println("req.readyState=${req.readyState.toInt()}")
 	  if (ReadyState.values()[req.readyState.toInt()] == DONE) {
 		println("doing callback")
-		callback(req._getResponses())
+		callback(req.getResponses())
 	  }
 	}
 	req.send()
   }
 
   inline fun <reified D> send(sendData: D): T {
-	val req = _setup(async = false)
+	val req = setup(async = false)
 	req.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
 	req.send(Json.encodeToString(sendData))
-	return req._getResponses()
+	return req.getResponses()
   }
 
   inline fun <reified D> sendAsync(sendData: D, noinline callback: (T)->Unit) {
-	val req = _setup(async = true)
+	val req = setup(async = true)
 	req.setOnReadyStateChange {
 	  if (ReadyState.values()[req.readyState.toInt()] == DONE) {
-		callback(req._getResponses())
+		callback(req.getResponses())
 	  }
 	}
 	req.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
