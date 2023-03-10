7ab443941e0a04b561acc54db6401d1738b447c5
mgroth0 <mgroth49@gmail.com>
2023-03-10 15:57:23 -0500

    autocommit

--- src/commonMain/kotlin/matt/http/api/api.kt
+++ src/commonMain/kotlin/matt/http/api/api.kt
@@ -1,18 +1,23 @@
 package matt.http.api
 
+import matt.http.connection.HTTPConnection
 import matt.http.headers.HTTPHeaders
 import matt.http.headers.headers
 import matt.http.http
 import matt.http.req.MutableHTTPRequest
 import matt.http.url.MURL
 
-open class API(
+interface API {
+  suspend fun http(url: String, op: MutableHTTPRequest.()->Unit = {}): HTTPConnection
+}
+
+open class APIImpl(
   val urlPrefix: MURL
-) {
+): API {
 
   open val defaultHeaders: (HTTPHeaders.()->Unit)? = null
 
-  suspend fun http(url: String, op: MutableHTTPRequest.()->Unit = {}) = urlPrefix.resolve(url).http {
+  override suspend fun http(url: String, op: MutableHTTPRequest.()->Unit) = urlPrefix.resolve(url).http {
 	headers {
 	  defaultHeaders?.invoke(this)
 	}
