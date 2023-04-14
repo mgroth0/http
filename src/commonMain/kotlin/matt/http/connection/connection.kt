349ac3ce0f2a2aa69ce174149b36278dad96a592
mgroth0 <mgroth49@gmail.com>
2023-04-14 13:41:57 -0400

    changed src/commonMain/kotlin/matt/http/connection/connection.kt

--- src/commonMain/kotlin/matt/http/connection/connection.kt
+++ src/commonMain/kotlin/matt/http/connection/connection.kt
@@ -1,10 +1,9 @@
 package matt.http.connection
 
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsChannel
-import io.ktor.client.statement.readBytes
-import io.ktor.http.HttpStatusCode
-import io.ktor.utils.io.readUTF8Line
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.utils.io.*
+import io.ktor.utils.io.errors.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
@@ -13,57 +12,57 @@ sealed interface HTTPConnectResult
 
 suspend fun HTTPConnectResult.requireSuccessful() = (this as HTTPConnection).requireSuccessful()
 
-abstract class HTTPConnectionProblem(message: String): Exception(message), HTTPConnectResult
+abstract class HTTPConnectionProblem(message: String) : IOException(message), HTTPConnectResult
 
-class HTTPConnection(private val response: HttpResponse): HTTPConnectResult {
-
-
-  private var alreadyReadBytes: ByteArray? = null
-  private val mutex = Mutex()
-
-  suspend fun bytes(): ByteArray {
-	mutex.withLock {
-	  if (alreadyReadBytes == null) {
-		alreadyReadBytes = response.readBytes()
-	  }
-	  return alreadyReadBytes!!
-	}
-  }
-
-  suspend fun text(): String {
-	return bytes().decodeToString()
-  }
-
-  private var alreadyGotHTTPStatusCode: HttpStatusCode? = null
-  private val statusMutex = Mutex()
-
-  suspend fun statusCode(): HttpStatusCode {
-	statusMutex.withLock {
-	  if (alreadyGotHTTPStatusCode == null) {
-		alreadyGotHTTPStatusCode = response.status
-	  }
-	  return alreadyGotHTTPStatusCode!!
-	}
-  }
-
-  suspend fun requireSuccessful(): HTTPConnection {
-	bytes()
-	return this
-  }
-
-  suspend fun print() {
-	println(statusCode())
-	println(text())
-  }
+class HTTPConnection(private val response: HttpResponse) : HTTPConnectResult {
 
 
-  suspend fun consumeLines(op: (String)->Unit) {
-	val channel = response.bodyAsChannel()
-	do {
-	  val nextLine = channel.readUTF8Line()
-	  if (nextLine != null) op(nextLine)
-	} while (nextLine != null)
-  }
+    private var alreadyReadBytes: ByteArray? = null
+    private val mutex = Mutex()
+
+    suspend fun bytes(): ByteArray {
+        mutex.withLock {
+            if (alreadyReadBytes == null) {
+                alreadyReadBytes = response.readBytes()
+            }
+            return alreadyReadBytes!!
+        }
+    }
+
+    suspend fun text(): String {
+        return bytes().decodeToString()
+    }
+
+    private var alreadyGotHTTPStatusCode: HttpStatusCode? = null
+    private val statusMutex = Mutex()
+
+    suspend fun statusCode(): HttpStatusCode {
+        statusMutex.withLock {
+            if (alreadyGotHTTPStatusCode == null) {
+                alreadyGotHTTPStatusCode = response.status
+            }
+            return alreadyGotHTTPStatusCode!!
+        }
+    }
+
+    suspend fun requireSuccessful(): HTTPConnection {
+        bytes()
+        return this
+    }
+
+    suspend fun print() {
+        println(statusCode())
+        println(text())
+    }
+
+
+    suspend fun consumeLines(op: (String) -> Unit) {
+        val channel = response.bodyAsChannel()
+        do {
+            val nextLine = channel.readUTF8Line()
+            if (nextLine != null) op(nextLine)
+        } while (nextLine != null)
+    }
 
 }
 
