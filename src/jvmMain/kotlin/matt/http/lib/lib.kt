e0470b09e02f043e94933aed1f944075dda0644e
mgroth0 <mgroth49@gmail.com>
2023-04-20 11:42:26 -0400

    changed src/jvmMain/kotlin/matt/http/lib/lib.kt

--- src/jvmMain/kotlin/matt/http/lib/lib.kt
+++ src/jvmMain/kotlin/matt/http/lib/lib.kt
@@ -2,17 +2,8 @@
 
 package matt.http.lib
 
-import io.ktor.client.content.LocalFileContent
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.java.Java
-import io.ktor.http.content.OutgoingContent
-import io.ktor.utils.io.ByteWriteChannel
-import io.ktor.utils.io.jvm.javaio.copyTo
-import matt.file.MFile
-import matt.http.req.write.DuringConnectionWriter
-import matt.lang.anno.SeeURL
-import java.io.InputStream
-
+import io.ktor.client.engine.*
+import io.ktor.client.engine.java.*
 
 
 actual val httpClientEngine: HttpClientEngine by lazy {
